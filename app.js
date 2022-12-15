691606cc3c747ed998dfc2cbc0047a879b58e6ca
kkrish0511 <kkrishnakumar9497@gmail.com>
2022-12-15 09:49:27 +0530

    adding file

---
+++ app.js
@@ -0,0 +1,10 @@
+//!how to declare a class constructor in javascript?
+
+class emp{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+let e1 = new emp("ali", 26)
+console.log(e1)
